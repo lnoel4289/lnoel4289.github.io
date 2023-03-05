32743c5612f8d36057dc5cc1df9929b1b62da53b
Laurent NOËL <lnoel4289@gmail.com>
2023-03-05 16:00:42 +0100

    dropdown fonctionnel

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
-      {title}
-      {content}
+      <div
+        onClick={() => {
+          isOpen === true ? setIsOpen(false) : setIsOpen(true);
+        }}
+      >
+        {title}
+      </div>
+      {isOpen === true && <div>{content}</div>}
     </div>
   );
 };
