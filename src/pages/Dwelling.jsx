25960d0ad0a6d2ed88015ea023dc59a573cb685c
Laurent NOËL <lnoel4289@gmail.com>
2023-03-12 17:38:11 +0100

    404 redirection

--- src/pages/Dwelling.jsx
+++ src/pages/Dwelling.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import Carousel from "../components/Carousel";
 import Dropdown from "../components/Dropdown";
 import Error from "./Error";
@@ -10,30 +10,39 @@ import Header from "../layouts/Header";
 const Dwelling = () => {
   const { id } = useParams();
   const dwelling = dwellings.find((elem) => elem.id === id);
-  if (!dwelling) {
-    return <Error />;
-  }
 
-  return (
-    <><Header /><div className="dwelling">
-      <Carousel pictures={dwelling.pictures} title={dwelling.title} />
-      <Presentation
-        title={dwelling.title}
-        location={dwelling.location}
-        tags={dwelling.tags}
-        rating={dwelling.rating}
-        host={dwelling.host} />
-      <div className="dwelling__dropdowns">
-        <Dropdown title="Description" content={<p>{dwelling.description}</p>} />
-        <Dropdown
-          title="Équipements"
-          content={<ul>
-            {dwelling.equipments.map((eq, index) => {
-              return <li key={`${index}${eq}`}>{eq}</li>;
-            })}
-          </ul>} />
+  return dwelling ? (
+    <>
+      <Header />
+      <div className="dwelling">
+        <Carousel pictures={dwelling.pictures} title={dwelling.title} />
+        <Presentation
+          title={dwelling.title}
+          location={dwelling.location}
+          tags={dwelling.tags}
+          rating={dwelling.rating}
+          host={dwelling.host}
+        />
+        <div className="dwelling__dropdowns">
+          <Dropdown
+            title="Description"
+            content={<p>{dwelling.description}</p>}
+          />
+          <Dropdown
+            title="Équipements"
+            content={
+              <ul>
+                {dwelling.equipments.map((eq, index) => {
+                  return <li key={`${index}${eq}`}>{eq}</li>;
+                })}
+              </ul>
+            }
+          />
+        </div>
       </div>
-    </div></>
+    </>
+  ) : (
+    <Navigate to="/error" />
   );
 };
 
